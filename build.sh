baebf506f73cc2373ce54ca8e27cb71fe390c747
MountComb <69704258+MountComb@users.noreply.github.com>
2023-12-09 07:57:45 +0000

    improve build script

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ base=$(dirname $0)
 overlay_base=$base/DisplayCutoutEmulationInvisibleOverlay
 module_base=$base/module
 
-aapt2 compile --dir $overlay_base/res/ -o res.zip
-aapt2 link --manifest $overlay_base/AndroidManifest.xml res.zip --warn-manifest-validation -I $overlay_base/android-31.jar -o linked.apk
-zipalign -f 4 linked.apk aligned.apk
-keytool -genkey -keystore keystore.jks -storepass 123456 -keyalg RSA -keysize 2048 -dname 'CN=localhost' -validity 3650
-apksigner sign --ks keystore.jks --ks-pass pass:123456 --out signed.apk aligned.apk
-rm res.zip linked.apk aligned.apk keystore.jks
+aapt2 compile -v --dir $overlay_base/res/ -o res.zip
+aapt2 link -v --manifest $overlay_base/AndroidManifest.xml res.zip -I $overlay_base/android-31.jar -o linked.apk
+zipalign -v -f 4 linked.apk aligned.apk
+test -f keystore.jks || keytool -genkey -v -keystore keystore.jks -storepass 123456 -keyalg RSA -keysize 2048 -dname 'CN=localhost' -validity 3650
+apksigner sign -v --ks keystore.jks --ks-pass pass:123456 --out signed.apk aligned.apk
+rm res.zip linked.apk aligned.apk # keystore.jks
 
 cp -r $module_base ./
 mkdir -p module/system/product/overlay/DisplayCutoutEmulationInvisible
